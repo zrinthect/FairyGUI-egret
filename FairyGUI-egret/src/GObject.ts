44b7bc47c7a2ff1fa02dfc8a1edae282418619ef
xuej <zrinthect@gmail.com>
2019-06-15 16:45:57 +0800

    修改.matrix为.$getMatrix()，因为egret.DisplayObject.matrix每次都会new 一个 Matrix; 优化性能，减少对象创建;

--- FairyGUI-egret/src/GObject.ts
+++ FairyGUI-egret/src/GObject.ts
@@ -353,7 +353,7 @@ module fairygui {
                 if (this._pivotX != 0 || this._pivotY != 0) {
                     var px: number = this._pivotX * this._width;
                     var py: number = this._pivotY * this._height;
-                    var pt: egret.Point = this._displayObject.matrix.transformPoint(px, py, GObject.sHelperPoint);
+                    var pt: egret.Point = this._displayObject.$getMatrix().transformPoint(px, py, GObject.sHelperPoint);
                     this._pivotOffsetX = this._pivotX * this._width - (pt.x - this._displayObject.x);
                     this._pivotOffsetY = this._pivotY * this._height - (pt.y - this._displayObject.y);
                 }
